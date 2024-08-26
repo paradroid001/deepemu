7db05d758a48cf0d3833930f8cee01c44b8c23aa
washyking <maiblefive201@gmail.com>
2024-08-26 15:28:31 +0800

    new functions for gui

--- tk/tk.py
+++ tk/tk.py
@@ -1,26 +1,46 @@
 from tkinter import *
+from tkinter import filedialog
 
 #write input to terminal
 def submit():
     usr = entry.get()
     print(usr)
 
-#write input from gui
+#delete input from gui
 def clear():
     entry.delete(0,END)
 
+# write input from gui to a txt file
+def save_input():
+    usr = entry.get()
+    with open("input.txt", "w") as file:
+        file.write(usr)
+    clear()
 
+#recieve file upload from user
+def upload():
+    file = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+    if not file.endswith('.txt'):
+        return
+    print("selected:", file)
 
+#initialise gui window
 window = Tk()
-window.geometry("150x150")
+window.geometry("250x250")
 window.title("DesertEMU")
-entry = Entry()
-entry.pack()
 
-submit = Button(window, text="submit", command=submit)
+#upload buton
+upload_button = Button(window, text="Upload", command=upload)
+upload_button.pack(pady=5)
+
+#entry field
+entry = Entry()
+entry.pack(pady=5)
+
+#submit button
+submit = Button(window, text="submit", command=save_input)
 submit.pack(side=BOTTOM)
 
-clear = Button(window, text="clear", command=clear)
-clear.pack(side=BOTTOM)
 
+#persistence
 window.mainloop()
