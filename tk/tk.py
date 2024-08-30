40b11564598ebe3916dc8319c6412f339a292c65
Alvin Odunga <101785858+washyking@users.noreply.github.com>
2024-08-30 17:44:16 +1000

    Retro Tkinter gui
    going to read the documentation for the custom tkinter (ctk) to get an updated more modern version of the gui

--- tk/tk.py
+++ tk/tk.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
 
+
 #write input to terminal
 def submit():
     usr = entry.get()
@@ -13,8 +14,12 @@ def clear():
 # write input from gui to a txt file
 def save_input():
     usr = entry.get()
-    with open("input.txt", "w") as file:
-        file.write(usr)
+    usr1 = clicked.get()
+    #usr2 = dir.get()
+    with open("input.txt" , "w") as file:
+        file.write(usr + "\n")
+        file.write(usr1 + "\n")
+        #file.write(usr2,'\n')
     clear()
 
 #recieve file upload from user
@@ -33,25 +38,26 @@ def select_dir():
         libchoice.config(text=directory) 
 
 
+# Dropdown menu options 
+options = [ 
+    "x86", 
+    "ARM", 
+    "MIPS", 
+    "PowerPc", 
+    "x64"
+]
+
+
 #initialise gui window
 window = Tk()
-window.geometry("250x350")
-window.title("DesertEMU")
-
-#upload buton
-upload_button = Button(window, text="Upload config file", command=upload)
-upload_button.pack(pady=5)
-
-#entry field
-entry = Entry()
-entry.pack(pady=5)
-
-##ARCHITECTURE SELECTION
-arch = Comb
-
+window.geometry("350x350")
+window.title("DesertEMU"
+)
 
 ##DIRECTORY SELECTION SECTION
-dir = Button(window, text="Select dir", command=select_dir)
+reside_dir = Label(window, text="Select residing directory", wraplength=400, justify="left")
+reside_dir.pack(pady=5)
+dir = Button(window, text="Open Folder", command=select_dir)
 dir.pack(pady=5)
 
 libchoice = Label(window, text="No directory selected", wraplength=400, justify="left")
@@ -59,9 +65,36 @@ libchoice.pack(pady=5)
 
 
 
+
+
+##ARCHITECTURE SELECTION
+arch_label = Label(window, text="Select your required architecture")
+arch_label.pack(pady=5)
+
+
+# Create Dropdown menu 
+# initial menu text 
+# datatype of menu text 
+clicked = StringVar() 
+clicked.set("") 
+arch = OptionMenu( window , clicked , *options ) 
+arch.pack() 
+
+#CONTAINER NAMING CONVENTIONS
+con_name = Label(window, text="Whats the prefix names of the container", wraplength=400, justify="left")
+con_name.pack(pady=5)
+entry = Entry()
+entry.pack(pady=5)
+
+
+#upload buton
+upload_button = Button(window, text="Upload config file", command=upload)
+upload_button.pack(side=BOTTOM,pady=5)
+
+
 ##SUBMIT BUTTON SECTION
-submit = Button(window, text="submit", command=save_input)
-submit.pack(side=BOTTOM)
+submit = Button(window, text="Create config file", command=save_input)
+submit.pack(side=BOTTOM, pady=5) 
 
 
 #persistence
