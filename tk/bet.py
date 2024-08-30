008b038b8a325da4c65b65a202eacdfe0fab98ef
Alvin Odunga <101785858+washyking@users.noreply.github.com>
2024-08-30 18:04:18 +1000

    Created gui w/ custom library

--- tk/bet.py
+++ tk/bet.py
@@ -1,12 +1,35 @@
-import customtkinter
+import customtkinter 
+from tkinter import *
+from tkinter import filedialog
 
 def button_callback():
     print("button clicked")
 
+#write input to terminal
+def submit():
+    usr = entry.get()
+    print(usr)
+
+#delete input from gui
+def clear():
+    entry.delete(0,END)
+    
+def select_dir():
+    directory = filedialog.askdirectory()  # Open a dialog to select a directory
+    if directory:
+        libchoice.config(text=directory) 
+
 app = customtkinter.CTk()
 app.geometry("400x150")
 
-button = customtkinter.CTkButton(app, text="my button", command=button_callback)
+entry = customtkinter.CTkEntry(app, placeholder_text="CTkEntry")
+entry.pack(padx=20, pady=20)
+
+
+#upload buton
+button = customtkinter.CTkButton(app, text="CLEAR", command=clear)
 button.pack(padx=20, pady=20)
+
+
 
 app.mainloop()
